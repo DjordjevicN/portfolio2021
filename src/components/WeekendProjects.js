5fd9067c86a03a0f5d1c1399de1cc8d77e952294
nikola djordjevic <nikola.dj.87@gmail.com>
2021-05-06 15:01:01 +0200

    6.4 first of weekprojects

--- src/components/WeekendProjects.js
+++ src/components/WeekendProjects.js
@@ -1,10 +1,42 @@
 import React from 'react';
-
+import Navigation from './Navigation'
+import { FaGithub } from "react-icons/fa";
 function WeekendProjects() {
     return (
-        <div>
-            weekend
+        <div className="weekendProjects__wrapper">
+            <Navigation />
+            <div className='weekendProjects__content'>
+                <h1>WEEKEND PROJECTS</h1>
+
+                <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/paralax-red-mountin.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>Parallax mountain</h3>
+                        <p>Found this project idea on pinterest and had to replicate it</p>
+                        <button className='button'><a href="https://github.com/DjordjevicN/PurpleMountainParalax" target="_blank" rel="noreferrer">Github</a></button>
+                        <button className='button'><a href="http://parallaxredmountain.nikola-djordjevic.com/" target="_blank" rel="noreferrer">Visit</a></button>
+                    </div>
+                </div>
+
+
+
+                <div>paralax purple mountin</div>
+                <div>paralax natural</div>
+                <div>cyberpunk portfolio</div>
+                <div>ecommerce</div>
+                <div>digital</div>
+                <div>belgrade galery</div>
+                <div>purple portfolio</div>
+                <div>memory card</div>
+                <div>rave</div>
+                <div>cybp</div>
+                <div>loto</div>
+                <div>movie</div>
+                <div>piza</div>
+                <div>whatsup mockup</div>
+            </div>
         </div>
+
     );
 }
 
