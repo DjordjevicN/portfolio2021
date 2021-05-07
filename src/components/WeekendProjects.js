bd3ab58a5283d7b0bdebdc61e90da0b91d8e00b0
nikola djordjevic <nikola.dj.87@gmail.com>
2021-05-07 21:01:08 +0200

    adding week projects 1

--- src/components/WeekendProjects.js
+++ src/components/WeekendProjects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from './Navigation'
-import { FaGithub } from "react-icons/fa";
+
 function WeekendProjects() {
     return (
         <div className="weekendProjects__wrapper">
@@ -13,27 +13,76 @@ function WeekendProjects() {
                     <div className="textAction">
                         <h3>Parallax mountain</h3>
                         <p>Found this project idea on pinterest and had to replicate it</p>
+                        <p>Parallax effect with Javascript</p>
                         <button className='button'><a href="https://github.com/DjordjevicN/PurpleMountainParalax" target="_blank" rel="noreferrer">Github</a></button>
                         <button className='button'><a href="http://parallaxredmountain.nikola-djordjevic.com/" target="_blank" rel="noreferrer">Visit</a></button>
                     </div>
                 </div>
 
+                <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/cyberpunk.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>Portfolio 2020</h3>
+                        <p>This project is second attempt to make my portfolio. I wanted to make it to be like cyberpunk theme</p>
+
+                        <button className='button'><a href="https://github.com/DjordjevicN/Cyberpunk_portfolio_2020" target="_blank" rel="noreferrer">Github</a></button>
+
+                        <button className='button'><a href="http://nikola-djordjevic.com/" target="_blank" rel="noreferrer">Visit</a></button>
 
 
-                <div>paralax purple mountin</div>
-                <div>paralax natural</div>
-                <div>cyberpunk portfolio</div>
-                <div>ecommerce</div>
-                <div>digital</div>
-                <div>belgrade galery</div>
-                <div>purple portfolio</div>
-                <div>memory card</div>
-                <div>rave</div>
-                <div>cybp</div>
-                <div>loto</div>
-                <div>movie</div>
-                <div>piza</div>
-                <div>whatsup mockup</div>
+                    </div>
+                </div>
+
+                <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/parallax-nature.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>Parallax Explore</h3>
+                        <p>Found this project idea on dribbble and had to replicate it</p>
+                        <p>Parallax effect with GSAP</p>
+                        <button className='button'><a href="https://github.com/DjordjevicN/PurpleMountainParalax" target="_blank" rel="noreferrer">Github</a></button>
+
+                        <button className='button'><a href="http://parallaxnature.nikola-djordjevic.com/" target="_blank" rel="noreferrer">Visit</a></button>
+
+                        <button className='button'><a href="https://dribbble.com/shots/5885777-Herschel-Destination-Unknown" target="_blank" rel="noreferrer">Idea</a></button>
+                    </div>
+                </div>
+
+                <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/rave-party.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>One minute rave</h3>
+                        <p>This was just an idea that was fun at the time </p>
+
+                        <button className='button'><a href="https://github.com/DjordjevicN/Rave-event-frontend" target="_blank" rel="noreferrer">Github</a></button>
+
+                        <button className='disabled'>Visit</button>
+                    </div>
+                </div>
+
+                <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/cybp.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>Portfolio 2019</h3>
+                        <p>This project is first attempt to make my portfolio. I wanted to make it to be like cyberpunk theme</p>
+
+                        <button className='button'><a href="https://github.com/DjordjevicN/Djordjevic-Nikola-Portfolio-CYBP" target="_blank" rel="noreferrer">Github</a></button>
+
+                        <button className='disabled'>Visit</button>
+                    </div>
+                </div>
+
+                <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/movie.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>Movie Review</h3>
+                        {/* <p>This project is first attempt to make my portfolio. I wanted to make it to be like cyberpunk theme</p> */}
+
+                        <button className='button'><a href="https://github.com/DjordjevicN/Movie-Search-API-" target="_blank" rel="noreferrer">Github</a></button>
+
+                        <button className='disabled'>Visit</button>
+                    </div>
+                </div>
+
             </div>
         </div>
 
