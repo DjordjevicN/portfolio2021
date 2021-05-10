d133e0519c87942698c3e0d541bfa2f84d40dec1
nikola djordjevic <nikola.dj.87@gmail.com>
2021-05-10 03:19:08 +0200

    added rick and morty project

--- src/components/WeekendProjects.js
+++ src/components/WeekendProjects.js
@@ -9,6 +9,16 @@ function WeekendProjects() {
                 <h1>WEEKEND PROJECTS</h1>
 
                 <div className="weekendSplit">
+                    <img className='weekendProject__image' src="/images/rickandmorty.jpg" alt="" />
+                    <div className="textAction">
+                        <h3>Rick and Morty show character facts</h3>
+                        <p>Exploring the NextJS </p>
+                        <button className='button'><a href="https://github.com/DjordjevicN/RickAndMortyChars" target="_blank" rel="noreferrer">Github</a></button>
+                        <button className='button'><a href="https://rick-and-morty-chars.vercel.app/" target="_blank" rel="noreferrer">Visit</a></button>
+                    </div>
+                </div>
+
+                <div className="weekendSplit">
                     <img className='weekendProject__image' src="/images/paralax-red-mountin.jpg" alt="" />
                     <div className="textAction">
                         <h3>Parallax mountain</h3>
